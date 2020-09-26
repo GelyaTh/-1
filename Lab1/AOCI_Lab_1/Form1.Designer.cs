9f90720451eedd3e38cb70f118a275b13fb14a31
GelyaTh <47592184+GelyaTh@users.noreply.github.com>
2020-09-26 16:26:14 +0700

    Add files via upload

---
+++ Lab1/AOCI_Lab_1/Form1.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace AOCI_Lab_1
+{
+    partial class Form1
+    {
+       
+        private System.ComponentModel.IContainer components = null;
+
+       
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.imageBox1 = new Emgu.CV.UI.ImageBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.imageBox2 = new Emgu.CV.UI.ImageBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.trackBar3 = new System.Windows.Forms.TrackBar();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button6 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // imageBox1
+            // 
+            this.imageBox1.Location = new System.Drawing.Point(12, 12);
+            this.imageBox1.Name = "imageBox1";
+            this.imageBox1.Size = new System.Drawing.Size(640, 480);
+            this.imageBox1.TabIndex = 2;
+            this.imageBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(668, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(83, 37);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Картинка";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // imageBox2
+            // 
+            this.imageBox2.Location = new System.Drawing.Point(764, 12);
+            this.imageBox2.Name = "imageBox2";
+            this.imageBox2.Size = new System.Drawing.Size(640, 480);
+            this.imageBox2.TabIndex = 4;
+            this.imageBox2.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(668, 99);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(83, 37);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Фильтр Канни";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 33;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(668, 412);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(83, 37);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Видео";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.Button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(668, 263);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(83, 37);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Пороговый фильтр";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(668, 455);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(83, 37);
+            this.button5.TabIndex = 9;
+            this.button5.Text = "Обработка";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(658, 161);
+            this.trackBar1.Minimum = 1;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(104, 45);
+            this.trackBar1.TabIndex = 10;
+            this.trackBar1.Value = 1;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Location = new System.Drawing.Point(658, 212);
+            this.trackBar2.Minimum = 1;
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(104, 45);
+            this.trackBar2.TabIndex = 11;
+            this.trackBar2.Value = 1;
+            this.trackBar2.Scroll += new System.EventHandler(this.trackBar2_Scroll);
+            // 
+            // trackBar3
+            // 
+            this.trackBar3.Location = new System.Drawing.Point(658, 306);
+            this.trackBar3.Maximum = 12;
+            this.trackBar3.Minimum = 4;
+            this.trackBar3.Name = "trackBar3";
+            this.trackBar3.Size = new System.Drawing.Size(104, 45);
+            this.trackBar3.TabIndex = 12;
+            this.trackBar3.Value = 4;
+            this.trackBar3.Scroll += new System.EventHandler(this.trackBar3_Scroll);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.Menu;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Location = new System.Drawing.Point(658, 142);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(104, 13);
+            this.textBox1.TabIndex = 13;
+            this.textBox1.Text = "     cannyThreshold";
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.Menu;
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.Location = new System.Drawing.Point(658, 193);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(104, 13);
+            this.textBox2.TabIndex = 14;
+            this.textBox2.Text = "cannyThresholdLinking";
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(686, 383);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(45, 23);
+            this.button6.TabIndex = 15;
+            this.button6.Text = "replay";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1416, 548);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.trackBar3);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.imageBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.imageBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Emgu.CV.UI.ImageBox imageBox1;
+        private System.Windows.Forms.Button button1;
+        private Emgu.CV.UI.ImageBox imageBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.TrackBar trackBar3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button6;
+    }
+}
+
---
+++ Lab1/AOCI_Lab_1/Form1.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+using Emgu.CV.Util;
+
+
+namespace AOCI_Lab_1
+{
+    public partial class Form1 : Form
+    {
+        private Image<Bgr, byte> sourceImage; //<Цветовое пространство(rgb), глубина цвета>
+        private VideoCapture capture;
+        private string videoFileName;
+        int frameCounter = 0;
+        bool isVideoFiltering = false;
+
+        double cannyThreshold = 10.0;
+        double cannyThresholdLinking = 10.0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Картинка
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Файлы изображений (*.jpg,  *.jpeg,  *.jpe,  *.jfif,  *.png)  |  *.jpg;  *.jpeg;  *.jpe;  *.jfif; *.png";
+            var result = openFileDialog.ShowDialog(); // открытие диалога выбора файла
+
+            if (result == DialogResult.OK) // открытие выбранного файла
+            {
+                string fileName = openFileDialog.FileName;
+                sourceImage = new Image<Bgr, byte>(fileName).Resize(640, 480, Inter.Linear);
+
+                imageBox1.Image = sourceImage;
+            }
+        }
+
+        //Метод примененяет фильтр Канни к картинке и возвращает картинку с фильтром
+        private Image<Gray, byte> GetImageWithCanny()
+        {
+            Image<Gray, byte> grayImage = sourceImage.Convert<Gray, byte>();
+
+            var tempImage = grayImage.PyrDown();
+            var destImage = tempImage.PyrUp();
+
+            Image<Gray, byte> cannyEdges = destImage.Canny(cannyThreshold, cannyThresholdLinking);
+
+            return cannyEdges;
+        }
+
+        //Метод порогового фильтра
+        private Image<Bgr, byte> GetImageWithCellShading(int level)
+        {
+            Image<Gray, byte> cannyEdges = GetImageWithCanny();
+
+            var cannyEdgesBgr = cannyEdges.Convert<Bgr, byte>();
+            var resultImage = sourceImage.Sub(cannyEdgesBgr); // попиксельное вычитание
+
+            //обход по каналам
+            for (int channel = 0; channel < resultImage.NumberOfChannels; channel++)
+                for (int x = 0; x < resultImage.Width; x++)
+                    for (int y = 0; y < resultImage.Height; y++) // обход по пискелям
+                    {
+                        // получение цвета пикселя
+                        byte color = resultImage.Data[y, x, channel];
+
+                        for (int interval = 0; interval < level; interval++)
+                        {
+                            if ((color > (255 / level) * interval) && (color <= (255 / level) * (interval + 1)))
+                            {
+                                color = (byte)((255 / level) * interval);
+                                break;
+                            }
+                        }
+
+                        resultImage.Data[y, x, channel] = color; // изменение цвета пикселя
+                    }
+
+            return resultImage;
+        }
+        
+        //Фильтр Канни
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            imageBox2.Image = GetImageWithCanny();
+
+            //расширение светлых областей(иттерации)
+            // cannyEdges._Dilate(1);
+        }
+
+        //Пороговый фильтр
+        private void button3_Click(object sender, EventArgs e)
+        {
+            imageBox2.Image = GetImageWithCellShading(trackBar3.Value);
+        }
+
+        //Webcam
+        //private void Button3_Click(object sender, EventArgs e)
+        //{
+        //    // инициализация веб-камеры
+        //    capture = new VideoCapture();
+        //    capture.ImageGrabbed += ProcessFrame;
+        //    capture.Start(); // начало обработки видеопотока
+        //}
+
+        // захват кадра из видеопотока
+        //private void ProcessFrame(object sender, EventArgs e)
+        //{
+        //    var frame = new Mat();
+        //    capture.Retrieve(frame); // получение текущего кадра
+
+        //    Image<Bgr, byte> image = frame.ToImage<Bgr, byte>();
+
+        //    Image<Gray, byte> grayImage = image.Convert<Gray, byte>();
+
+        //    var tempImage = grayImage.PyrDown();
+        //    var destImage = tempImage.PyrUp();
+
+        //    double cannyThreshold = 10.0;
+        //    double cannyThresholdLinking = 40.0;
+        //    Image<Gray, byte> cannyEdges = destImage.Canny(cannyThreshold, cannyThresholdLinking);
+
+        //    imageBox2.Image = cannyEdges;
+        //}
+
+        //Video
+        private void Button4_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Файлы видео (*.webm,  *.mp4)  |  *.webm;  *.mp4";
+            var result = openFileDialog.ShowDialog(); // открытие диалога выбора файла
+
+            if (result == DialogResult.OK) // открытие выбранного файла
+            {
+                string fileName = openFileDialog.FileName;
+                videoFileName = fileName;
+                capture = new VideoCapture(fileName);               
+                timer1.Enabled = true;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            var frame = capture.QueryFrame();
+            sourceImage = frame.ToImage<Bgr, byte>();
+            imageBox1.Image = sourceImage;
+
+            if (isVideoFiltering)
+                imageBox2.Image = GetImageWithCellShading(trackBar3.Value);
+                //imageBox2.Image = GetImageWithCanny();
+            frameCounter++;
+
+            if (frameCounter >= capture.GetCaptureProperty(CapProp.FrameCount))
+            {
+                timer1.Enabled = false;
+                frameCounter = 0;
+            }
+                
+        }
+
+        //Обработка
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (isVideoFiltering)
+                isVideoFiltering = false;
+            else
+                isVideoFiltering = true;
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            cannyThreshold = 10.0 * trackBar1.Value;
+            imageBox2.Image = GetImageWithCanny();
+        }
+
+        private void trackBar2_Scroll(object sender, EventArgs e)
+        {
+            cannyThresholdLinking = 10.0 * trackBar2.Value;
+            imageBox2.Image = GetImageWithCanny();
+        }
+
+        private void trackBar3_Scroll(object sender, EventArgs e)
+        {
+            imageBox2.Image = GetImageWithCellShading(trackBar3.Value);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (capture is null) return;
+            frameCounter = 0;
+            capture = new VideoCapture(videoFileName);
+            timer1.Enabled = true;
+        }
+    }
+}
